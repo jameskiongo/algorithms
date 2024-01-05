c810db3f3f2f9c269f5267c6deb45ca1f550c676
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-01-05 11:08:20 +0300

    update

---
+++ gatech/new.py
@@ -0,0 +1,19 @@
+class Name:
+    def __init__(self):
+        self.firstname = "james"
+        self.lastname = "kiongo"
+
+
+class Person:
+    def __init__(self):
+        self.name = Name()
+        self.eyecolor = "blue"
+        self.age = 1
+
+    def __str__(self):
+        print(name)
+
+
+person = Person()
+
+print(person.name)
